184e66d102d97fd3aec4a67878df4ba82a8e64b5
Abhinav Nigam <abhinavnigam2207@gmail.com>
2018-07-17 01:24:37 +0530

    Format, unformat, clear, copy functionalities implemented.

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,43 @@
+const path = require('path');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = {
-    mode: "none",
-    entry: './main.js',
-
-    output: {
-      path: '/',
-      filename: 'index.js'
-    },
-
-    devServer: {
-      inline: true,
-      port: 7777
-    },
-
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['es2015', 'react']
-          }
-        },
-        {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
+  mode: 'none',
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'index.js'
+  },
+  devServer: {
+    inline: true,
+    port: 7777
+  },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
         }
-      ]
-    },
-
-    resolve: {
-        extensions: [".js", ".json", ".jsx", ".css"],
-    },
-
-    performance: {
-      hints: false, // enum    maxAssetSize: 200000, // int (in bytes),
-      maxEntrypointSize: 400000, // int (in bytes)
-      assetFilter: function(assetFilename) {
-        // Function predicate that provides asset filenames
-        return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
       }
-    },
-
-    plugins: [
-        new LiveReloadPlugin()
     ]
-  }
+  },
+  performance: {
+    hints: process.env.NODE_ENV === 'production' ? "warning" : false, // enum    maxAssetSize: 200000, // int (in bytes),
+    maxEntrypointSize: 400000, // int (in bytes)
+    assetFilter: function(assetFilename) {
+      // Function predicate that provides asset filenames
+      return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+    }
+  },
+  plugins: [
+    new LiveReloadPlugin()
+  ]
+}
